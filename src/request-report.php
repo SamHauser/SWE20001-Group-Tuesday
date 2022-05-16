c157ffc7c1363d32c4d248fff1d0528d45683e89
rhysyounger <72488144+rhysyounger@users.noreply.github.com>
2022-05-16 13:54:12 +1000

    Add report pages

---
+++ src/request-report.php
@@ -0,0 +1,14 @@
+<?php include 'includes/header.inc'; ?>
+<body>
+    <?php include 'includes/menu.inc'; ?>
+    <h2>Request Report</h2>
+
+    <form method="post" action="show-report.php">
+    </form>
+
+    <?php
+    ?>
+
+    <?php include 'includes/footer.inc'; ?>
+</body>
+</html>
