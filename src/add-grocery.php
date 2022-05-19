e2fcbec9866d5dd2e3d24c2f8b94c22062d8f9c6
SAMUEL HAUSER <hausersm1@gmail.com>
2022-05-19 14:08:01 +1000

    updated add-grocery with price/quantity

--- src/add-grocery.php
+++ src/add-grocery.php
@@ -19,6 +19,14 @@
                 <textarea name="info" id="info" required rows="4" cols="50"></textarea>
             </p>
             <p>
+                <label for="quan">Quantity</label>
+                <input type="text" name="quan" id="quan" required />
+            </p>
+            <p>
+                <label for="price">Price</label>
+                <input type="text" name="price" id="price" required />
+            </p>
+            <p>
             <input type="submit" value="Submit">
             <input type="reset"> 
             </p>
@@ -45,12 +53,13 @@
             $c_id = mysqli_real_escape_string($conn, cleanInput($_POST["id"]));
             $c_name = mysqli_real_escape_string($conn, cleanInput($_POST["name"]));
             $c_info = mysqli_real_escape_string($conn, cleanInput($_POST["info"]));
-
+            $c_quan = mysqli_real_escape_string($conn, cleanInput($_POST["quan"]));
+            $c_price = mysqli_real_escape_string($conn, cleanInput($_POST["price"]));
             // add to the database
 
             $sql = 
-            "INSERT INTO ProductInformation (product_id, product_name, product_information)
-            VALUES ('$c_id', '$c_name', '$c_info')";
+            "INSERT INTO ProductInformation (product_id, product_name, product_information, quantity, price)
+            VALUES ('$c_id', '$c_name', '$c_info', '$c_quan', '$c_price')";
 
             if (mysqli_query($conn, $sql))
             {
